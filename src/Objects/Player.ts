679eb27ee05fbf0dc1dcff5ea1d9f1253339b5af
unknown <allenyby@udel.edu>
2023-04-28 22:37:38 -0400

    Updates

--- src/Objects/Player.ts
+++
@@ -1,8 +0,0 @@
-import Phaser from "phaser";
-
-export default class extends  Phaser.Physics.Arcade.Sprite{
-    constructor(scene: Phaser.Scene, xCord: number, yCord: number, texture: string, playerID: number) {
-        super(scene, xCord, yCord, texture, playerID);
-        scene.add.existing(this);
-    }
-}
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-	plugins: [],
-	server: { host: '0.0.0.0', port: 2332 },
-	clearScreen: false,
-	base: '/group-project-reflect/',
-})
