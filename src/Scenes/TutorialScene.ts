a44e621018c2542c69f2ba84faff5ee9f11ea21b
unknown <allenyby@udel.edu>
2023-05-15 14:18:39 -0400

    add arrow key on tour

--- src/Scenes/TutorialScene.ts
+++ src/Scenes/TutorialScene.ts
@@ -15,25 +15,28 @@ export default class TutorialScene extends BaseText {
         //this.addTextButton('Level 1', 'LevelTwo', 250)
         //this.addTextButton("Level 2", "LevelOne", 300)
         //this.addTextButton("Level 3", "LevelThree", 350)
-        
+
+
+        this.add.text(300, 200, "Move: use 4 arrow keys").setOrigin(0, 1);
+
         const gateImage = this.add.image(250, 250, 'gate');
         gateImage.setScale(0.5)
-        this.add.text(300, 250, "Gate: Clear these to make progress").setOrigin(0, 1);
+        this.add.text(300, 250, "Gate: block").setOrigin(0, 1);
         
         this.add.image(250, 300, 'switch');
-        this.add.text(300, 300, "Switch: Once hit, turns on forever").setOrigin(0, 1);
+        this.add.text(300, 300, "Switch: Hit (Not required)").setOrigin(0, 1);
         
         this.add.image(250, 350, 'button');
-        this.add.text(300, 350, "Button: Turns on only if stepped on").setOrigin(0, 1);
+        this.add.text(300, 350, "Button: Stepped on to active").setOrigin(0, 1);
         
         this.add.image(250, 400, 'dude');
-        this.add.text(300, 400, "Player: The player character").setOrigin(0, 1);
+        this.add.text(300, 400, "Player: you").setOrigin(0, 1);
         
         const boxImage = this.add.image(250, 450, 'box');
         boxImage.setScale(0.05)
-        this.add.text(300, 450, "Box: Jump on these to get to high places").setOrigin(0, 1);
+        this.add.text(300, 450, "Box: Jump on").setOrigin(0, 1);
 
-        this.add.text(280, 550, "Unlock the gates to get to the next level.").setOrigin(0, 1);
+        this.add.text(30, 550, "Hint the switch, active the button to unlock the gates then go to next level.");
     }
 
 }
